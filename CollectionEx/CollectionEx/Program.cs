7a4f00a02e7db8cc7b35966fb8807418b9078171
나래 윤 <naraeyoon90@naver.com>
2018-10-06 03:39:51 +0900

    자료구조 연습

---
+++ CollectionEx/CollectionEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue(1);
+            q.Enqueue(100);
+            q.Enqueue(200);
+            q.Dequeue();
+            q.Enqueue(22.5);
+            int aa = (int)q.Dequeue();
+            q.Enqueue("가나다");
+
+            Console.WriteLine(aa);
+            while (q.Count > 0)
+                Console.WriteLine(q.Dequeue());
+
+            // Stack : 후입선출 LIFO
+            // Push : 데이터 입력
+            // Pop : 데이터 출력
+        }
+    }
+}
